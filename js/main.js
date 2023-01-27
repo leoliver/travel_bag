f9679b1ac8d2fef4b6bad27366554530fe65dc9e
leoliver <77712075+leoliver@users.noreply.github.com>
2023-01-27 02:01:46 -0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+const formulario = document.querySelector('#novoItem')
+const lista = document.querySelector('#lista')
+let itens = JSON.parse(localStorage.getItem('itens')) || []
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+})
+
+formulario.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+    
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+    
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+    
+    if(existe) {
+        itemAtual.id = existe.id
+        
+        atualizaElemento(itemAtual)
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+    } else {
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0
+
+        criaElemento(itemAtual)
+        
+        itens.push(itemAtual)
+    }
+
+    localStorage.setItem('itens', JSON.stringify(itens))
+    
+    limpaTexto(nome)
+    limpaTexto(quantidade)
+})
+
+function criaElemento (item) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add('item')
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id= item.id
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += item.nome
+
+    novoItem.appendChild(botaoDeleta(item.id))
+    
+    lista.appendChild(novoItem)    
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement('button')
+    elementoBotao.innerText = "X"
+    elementoBotao.classList.add('botaoDelete')
+
+    elementoBotao.addEventListener('dblclick', function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(item, id) {
+    item.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem('itens', JSON.stringify(itens))
+}
+
+function limpaTexto (item) {
+    if(item.value.toString() !== "") {
+        item.value = ""
+    } else {
+        return
+    }
+}
+
+
